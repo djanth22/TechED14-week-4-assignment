0995136a698e44e72609ef8089c13a274d5af883
David Anthony <davywavy22@googlemail.com>
2024-09-20 13:28:39 +0100

    adjustments to client folder

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ export const db = new pg.Pool({
   connectionString: dbConnectionString,
 });
 
-const PORT = "https://teched14-week-4-assignment.onrender.com";
-app.listen(PORT, () => {
+const baseUrl = "https://teched14-week-4-assignment.onrender.com/";
+app.listen(baseUrl, () => {
   console.log(`server is running in port ${PORT}`);
 });
 
