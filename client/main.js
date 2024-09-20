98eae4885877706f704c52c4423d35fe71105d1b
David Anthony <davywavy22@googlemail.com>
2024-09-20 15:19:16 +0100

    modified handle submit in main.js

--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ function handleSubmit(event) {
   event.preventDefault();
   const formData = new FormData(form);
   const formValues = Object.fromEntries(formData);
-  fetch("https://teched14-week-4-assignment.onrender.com/form", {
+  fetch("http://localhost:8080/form", {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({ formValues }),
