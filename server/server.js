646d6ae76de0a4718cdb8267ddcdbbe54e6d458d
David Anthony <davywavy22@googlemail.com>
2024-09-21 23:30:25 +0100

    testing coors options

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ import pg from "pg";
 import cors from "cors";
 import dotenv from "dotenv";
 
+const corsOptions = {
+  origin: process.env.PORT,
+  optionsSuccessStatus: 200,
+};
+
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 export const dbConnectionString = process.env.DATABASE_URL;
 export const db = new pg.Pool({
   connectionString: dbConnectionString,
+  ssl: { rejectUnauthorized: false },
 });
 
 const port = process.env.PORT;
