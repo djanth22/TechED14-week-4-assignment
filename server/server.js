2411d28991fa212658520aad2aab5263c977a624
David Anthony <davywavy22@googlemail.com>
2024-09-22 17:55:16 +0100

    trying to sort this link out

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ export const db = new pg.Pool({
   connectionString: dbConnectionString,
 });
 
-const PORT = `https://teched14-week-4-assignment.onrender.com`;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server is running in port ${PORT}`);
 });
